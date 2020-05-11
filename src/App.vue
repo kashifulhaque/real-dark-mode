ba080ea4098c05992f120f0442bed8ede6cef932
kashifulhaque <haque.kashiful7@gmail.com>
2020-05-12 03:25:45 +0530

    👻 Initial commit A Real Dark Theme based on a reddit post
    Uses Reddit API to fetch from r/TwoSentenceHorror

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,108 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="overlay" @mousemove="shineLight"></div>
+
+    <h1>Real dark mode and r/TwoSentenceHorror</h1>
+
+    <br /><br /><br />
+
+    <div
+      class="story"
+      v-for="(horrorStory, i) in horrorStories"
+      :key="horrorStory.data.id"
+    >
+      <p>
+        {{ horrorStory.data.title }}
+      </p>
+
+      <p>
+        {{ horrorStory.data.selftext }}
+      </p>
+
+      <br />
+      <br />
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// Based on a Glitch project which I found on Reddit. Link: https://glitch.com/~paranoids-flashlight
+// Imports
+import axios from "axios";
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  data() {
+    return {
+      baseUrl: "https://www.reddit.com/r/TwoSentenceHorror.json?limit=25",
+      overlay: "",
+      horrorStories: [],
+    };
+  },
+  methods: {
+    shineLight(e) {
+      this.overlay.style.background = `radial-gradient(circle at ${e.pageX}px ${e.pageY}px, transparent 0%, black 8em`;
+    },
+  },
+  mounted() {
+    this.overlay = document.querySelector(".overlay");
+  },
+  created() {
+    axios
+      .get(this.baseUrl)
+      .then((res) => {
+        this.horrorStories = res.data.data.children;
+        this.horrorStories.shift(); // To remove the first pinned post from r/TwoSentenceHorror, this might break in the future
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  },
+};
 </script>
 
 <style>
+@import url("https://fonts.googleapis.com/css2?family=Major+Mono+Display&display=swap");
+
+* {
+  margin: 0;
+  padding: 0;
+  background-color: #131313;
+}
+
+html {
+  min-height: 100%;
+  position: relative;
+}
+
+body {
+  background: #f3f3f3;
+  height: 100%;
+  max-width: 640px;
+  margin: 2rem auto;
+}
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  color: #f3f3f3;
+}
+
+h1 {
+  font-family: "Major Mono Display", monospace;
+}
+
+p {
+  font-family: "Major Mono Display", monospace;
+}
+
+.overlay {
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  left: 0;
+  top: 0;
+  z-index: 100;
+  background: #1b1b1b;
+  overflow: hidden;
+  cursor: url("https://image.flaticon.com/icons/svg/107/107715.svg"), none;
 }
 </style>
